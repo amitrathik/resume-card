39099d6e72df45675d23758fb493dcb1b7164410
Amit Rathi <amit.rathi@c-e.com>
2020-01-06 13:28:42 -0500

    refactoring header into an organism component

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,15 +1,7 @@
 <template>
   <div id="app">
 
-    <header class="header">
-      <div class="header__left">
-        <Logo v-if="showLogo" /> 
-      </div>
-      
-      <div class="header__right">        
-        <ToggleTheme />
-      </div>
-    </header>
+    <Header />
 
     <main class="main">
       <slot/>
@@ -24,42 +16,16 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import ToggleTheme from '~/components/ToggleTheme.vue'
+import Header from '~/components/organisms/Header.vue'
 
 export default {
-  props: {
-    showLogo: { default: true }
-  },
   components: {
-    Logo,
-    ToggleTheme
+    Header
   }
 }
 </script>
 
 <style lang="scss">
-.header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  min-height: var(--header-height);
-  padding: 0 calc(var(--space) / 2);
-  top:0;
-  z-index: 10;
-
-  &__left,
-  &__right {
-    display: flex;
-    align-items: center;
-  }
-
-  @media screen and (min-width: 1300px) {
-    //Make header sticky for large screens
-    position: sticky;
-    width: 100%;
-  }
-}
 
 .main {
   margin: 0 auto;
