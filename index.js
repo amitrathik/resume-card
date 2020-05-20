b0abf334a36d38d4ea83bb87433214c37b1ebd28
amitrathik <amit.rathi.k@gmail.com>
2020-05-20 15:06:17 -0400

    removed server code since that's not what I need right now

--- index.js
+++ index.js
@@ -1,27 +1,7 @@
 const fs = require('fs');
-const http = require('http');
 const path = require('path');
-const hostname = '127.0.0.1';
-const port = 8080;
-
-const server = http.createServer(function (req,res){
-    
-}).listen(port,hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-});
 
 readFiles('./rscard');
-function promiseAllP(items, block) {
-    var promises = [];
-    items.forEach(function(item,index) {
-        promises.push( function(item,i) {
-            return new Promise(function(resolve, reject) {
-                return block.apply(this,[item,index,resolve,reject]);
-            });
-        }(item,index))
-    });
-    return Promise.all(promises);
-} //promiseAll
 
 function readFiles(dir) {
     return new Promise((resolve, reject) => {
